99084367cc1a276822ada8cbed2bb65f09ead0b8
RobertVxV <robertvinitchi@gmail.com>
2024-11-04 17:40:41 +0200

    splitted dog.hpp in dog.cpp and dog.hpp

--- homework1/inc/dog.hpp
+++ homework1/inc/dog.hpp
@@ -2,9 +2,6 @@
 #define DOG_HPP
 
 #include <string>
-#include <ctime>
-#include <iostream>
-#include <cstring>
 
 class Dog
 {
@@ -15,74 +12,31 @@ private:
 
 public:
     // Constructor
-    Dog(const char *name, int birth_year, bool isHealthy)
-    {
-        strcpy(this->name, name);
-        this->birth_year = birth_year;
-        this->isHealthy = isHealthy;
-        std::cout << "Created dog using constructor: " << this->to_string();
-    }
+    Dog(const char *name, int birth_year, bool isHealthy);
 
     // Destructor
-    ~Dog()
-    {
-        delete[] this->name;
-        std::cout << "Removed dog using destructor" << std::endl;
-    }
+    ~Dog();
 
     // Copy Constructor
-    Dog(const Dog &other)
-    {
-        strcpy(this->name, other.name); // deep copy
-        this->birth_year = other.birth_year;
-        this->isHealthy = other.isHealthy;
-        std::cout << "Created dog using copy constructor: " << this->to_string();
-    }
+    Dog(const Dog &other);
 
     // Move Constructor
-    Dog(Dog &&other) noexcept
-    {
-        delete[] this->name; // delete this->name
-        this->name = other.name; // transfer ownership
-        this->birth_year = other.birth_year;
-        this->isHealthy = other.isHealthy;
-        other.name = nullptr; // null other.name
-        std::cout << "Created dog using move constructor: " << this->to_string();
-    }
+    Dog(Dog &&other) noexcept;
 
     // Getters
-    char *getName() const { return name; }
-    int getAge() const
-    {
-        time_t t = time(nullptr);
-        struct tm *now = localtime(&t);
-        int current_year = now->tm_year + 1900;
-        return current_year - birth_year;
-    }
-    bool getIsHealthy() const { return isHealthy; }
+    char *getName() const;
+    int getAge() const;
+    bool getIsHealthy() const;
 
     // Setters
-    void setName(const char *name) const
-    {
-        if(this->name != nullptr){
-            strncpy(this->name, name, 19);
-        }
-    }
-    void setIsHealthy(const bool isHealthy) { this->isHealthy = isHealthy; }
+    void setName(const char *name) const;
+    void setIsHealthy(const bool isHealthy);
 
     // Actions
-    void bark() const
-    {
-        std::cout << this->name << ", age " << this->getAge() << " said: ham-ham" << std::endl;
-    }
+    void bark() const;
+
     // to_string method
-    std::string to_string() const
-    {
-        const char *name_ptr = (name != nullptr) ? name : "nullptr";
-        std::string name_str(name_ptr);
-        std::string health_status = isHealthy ? "healthy" : "not healthy";
-        return "Dog[name: " + name_str + ", age: " + std::to_string(getAge()) + ", health status: " + health_status + "]\n";
-    }
+    std::string to_string() const;
 };
 
 #endif // DOG_HPP
---
+++ homework1/src/dog.cpp
@@ -0,0 +1,78 @@
+#include <dog.hpp>
+#include <ctime>
+#include <iostream>
+#include <cstring>
+
+// Constructor
+Dog::Dog(const char *name, int birth_year, bool isHealthy)
+{
+    strcpy(this->name, name);
+    this->birth_year = birth_year;
+    this->isHealthy = isHealthy;
+    std::cout << "Created dog using constructor: " << this->to_string();
+}
+
+// Destructor
+Dog::~Dog()
+{
+    delete[] this->name;
+    std::cout << "Removed dog using destructor" << std::endl;
+}
+
+// Copy Constructor
+Dog::Dog(const Dog &other)
+{
+    strcpy(this->name, other.name); // deep copy
+    this->birth_year = other.birth_year;
+    this->isHealthy = other.isHealthy;
+    std::cout << "Created dog using copy constructor: " << this->to_string();
+}
+
+// Move Constructor
+Dog::Dog(Dog &&other) noexcept
+{
+    delete[] this->name;
+    this->name = other.name; // transfer ownership
+    this->birth_year = other.birth_year;
+    this->isHealthy = other.isHealthy;
+    other.name = nullptr; // null other.name
+    std::cout << "Created dog using move constructor: " << this->to_string();
+}
+
+// Getters
+char *Dog::getName() const { return name; }
+
+int Dog::getAge() const
+{
+    time_t t = time(nullptr);
+    struct tm *now = localtime(&t);
+    int current_year = now->tm_year + 1900;
+    return current_year - birth_year;
+}
+
+bool Dog::getIsHealthy() const { return isHealthy; }
+
+// Setters
+void Dog::setName(const char *name) const
+{
+    if(this->name != nullptr){
+        strncpy(this->name, name, 19);
+    }
+}
+
+void Dog::setIsHealthy(const bool isHealthy) { this->isHealthy = isHealthy; }
+
+// Actions
+void Dog::bark() const
+{
+    std::cout << this->name << ", age " << this->getAge() << " said: ham-ham" << std::endl;
+}
+
+// to_string method
+std::string Dog::to_string() const
+{
+    const char *name_ptr = (name != nullptr) ? name : "nullptr";
+    std::string name_str(name_ptr);
+    std::string health_status = isHealthy ? "healthy" : "not healthy";
+    return "Dog[name: " + name_str + ", age: " + std::to_string(getAge()) + ", health status: " + health_status + "]\n";
+}
